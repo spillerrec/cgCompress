6e9057cb593ca5e9b6ba59df46346cb8e65bb51e
spillerrec <spillerrec@hotmail.com>
2016-01-28 17:11:21 +0100

    ADD js-decoder: moved methods to a cgCompress object and prepared for custom compositing

--- js-decoder/main.js
+++ js-decoder/main.js
@@ -36,41 +36,53 @@ var stackParser = function( str ){
 									name: layer.getAttribute( "src" )
 								,	x: getAttrInt( layer, "x", 0 )
 								,	y: getAttrInt( layer, "y", 0 )
+								,	overlay: svgOverlay //TODO:
 								};
 						} );
 				} )
 		};
 };
 
-var drawStack = function( context, stack, zip, info ){ //TODO: avoid zip and info
-	var output = context.createImageData( info.width, info.height );
-	for( var i=0; i<output.data.length; i++ )
-		output.data[i] = 0;
-	
-	for( var i=stack.length-1; i>=0; i-- ){
-		var layer = stack[i];
-		var data2 = zip.files[layer.name].asUint8Array();
-		blendImages( output, loadImage( context, data2 ), layer.x, layer.y, svgOverlay );
-	}
-	context.putImageData( output, 0, 0 );
+var loadCgCompress = function( zip ){
+	var info = stackParser( zip.files["stack.xml"].asText() );
+	return {
+			zip: zip
+		,	width : info.width
+		,	height: info.height
+		,	stacks: info.stacks
+		
+		,	getImage: function( context, name ){
+					//TODO: Cache loaded images
+					return loadImage( context, this.zip.files[name].asUint8Array() );
+				}
+		,	drawStack: function( context, stack ){
+					var output = context.createImageData( this.width, this.height );
+					for( var i=0; i<output.data.length; i++ )
+						output.data[i] = 0;
+					
+					for( var i=stack.length-1; i>=0; i-- ){
+						var layer = stack[i];
+						blendImages( output, this.getImage( context, layer.name ), layer.x, layer.y, layer.overlay );
+					}
+					context.putImageData( output, 0, 0 );
+				}
+		};
 };
 
-JSZipUtils.getBinaryContent( 'test.cgcompress', function( err, data ){
-		if( err ){
-			alert( err );
-			throw err; // or handle err
-		}
-		
-		//Load zip, just testing with a single WebP image for now
-		var zip = new JSZip( data );
-		var info = stackParser( zip.files["stack.xml"].asText() );
-		console.log( info );
-		
-		var canvas = document.getElementById( "the-canvas" );
-		canvas.width  = info.width;
-		canvas.height = info.height;
-		
-		var context = canvas.getContext( "2d" );
-		
-		drawStack( context, info.stacks[1], zip, info );
-	} );
+JSZipUtils.getBinaryContent( 'test5.cgcompress', function( err, data ){
+	if( err ){
+		alert( err );
+		throw err; // or handle err
+	}
+	
+	var cg = loadCgCompress( new JSZip( data ) );
+	console.log( cg );
+	
+	var canvas = document.getElementById( "the-canvas" );
+	canvas.width  = cg.width;
+	canvas.height = cg.height;
+	
+	//Just testing with a single WebP image for now
+	var context = canvas.getContext( "2d" );
+	cg.drawStack( context, cg.stacks[14] );
+} );
